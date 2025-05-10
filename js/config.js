c0c6e7c1b557781d7ee4194cff20d8498e900647
ll@github <45592333+treasure-github@users.noreply.github.com>
2025-05-10 14:42:28 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,8 +13,8 @@ const PASSWORD_CONFIG = {
 // 网站信息配置
 const SITE_CONFIG = {
     name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    url: 'https://tv.wlink.cloudns.org',
+    description: '在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
     version: '1.0.3'
 };
@@ -214,4 +214,4 @@ const CUSTOM_API_CONFIG = {
 };
 
 // 新增隐藏内置黄色采集站API的变量，默认为true
-const HIDE_BUILTIN_ADULT_APIS = true;
+const HIDE_BUILTIN_ADULT_APIS = false;
